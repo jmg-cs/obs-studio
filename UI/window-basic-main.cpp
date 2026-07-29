ace27849df7cabdad6472d7a5851029ef927e254
JacobGrimmCreates <jacobgrimmbusiness@gmail.com>
2026-07-29 14:20:54 -0500

    UI: rename application title

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -8650,7 +8650,7 @@ void OBSBasic::UpdateTitleBar()
 	const char *profile = config_get_string(App()->GetUserConfig(), "Basic", "Profile");
 	const char *sceneCollection = config_get_string(App()->GetUserConfig(), "Basic", "SceneCollection");
 
-	name << "OBS ";
+	name << "Game Time Streaming Software ";
 	if (previewProgramMode)
 		name << "Studio ";
 
